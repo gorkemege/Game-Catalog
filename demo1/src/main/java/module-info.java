8e1186697a7f230981c79b3f43db42908f2902a3
Görkem Ege Zor <egezor4@gmail.com>
2025-04-23 16:08:06 +0300

    Delete demo1 directory

--- demo1/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.demo1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens org.example.demo1 to javafx.fxml;
-    exports org.example.demo1;
-}
